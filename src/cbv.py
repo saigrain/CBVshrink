c9510a34a07c5039035dc86a4ccef0c29ff41a28
Suzanne Aigrain <Suzanne.Aigrain@astro.ox.ac.uk>
2016-07-26 16:40:46 +0100

    cbv sel in progress

--- src/cbv.py
+++ src/cbv.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pylab as pl
 from VBLinRegARD import bayes_linear_fit_ard as VBF
-from cdpp import cdpp
+from stats import cdpp, medransig
 import astropy.io.fits as pyfits
 
 def fit_basis(flux, basis, scl = None):
@@ -90,7 +90,7 @@ def fixed_nb(flux, cbv, nB = 4, use = None, doPlot = True):
         pl.xlabel('Flux')
     return corrected_flux, weights
 
-def sel_nb(flux, cbv, nBmax = 8, use = None, doPlot = True):
+def sel_nb(flux, cbv, nBmax = None, use = None):
     '''
     corrected_flux = sel_nb(flux, basis, nBmax = 8, use = None, \
                             doPlot = True, full_output = False)
@@ -104,7 +104,6 @@ def sel_nb(flux, cbv, nBmax = 8, use = None, doPlot = True):
         nBmax: maximum number of CBVs to use (starting with the first)
         use: boolean array, True for data points to use in evaluating correction, 
              False for data points to ignore (NaNs are also ignored)
-        doPlot: set to False to suppress plot
         full_output: set to also return values for different numbers of CBVs
     Outputs:
         corrected_flux: (same shape as flux) corrected light curves
@@ -114,30 +113,57 @@ def sel_nb(flux, cbv, nBmax = 8, use = None, doPlot = True):
         
         If full_output is True, then additional 2-D arrays containing corrected fluxes and weights for 1, 2, ..., nBmax CBVs are alo returned 
     '''
-    nobs = len(flux)    
-    if cbv.shape[1] == nobs: cbv_ = cbv[:nB,:]
-    else: cbv_ = cbv[:,:nB].T
+    nobs = len(flux)
+    if cbv.shape[1] == nobs: cbv_ = np.copy(cbv)
+    else: cbv_ = cbv.T
+    if nBmax is None: nBmax = cbv.shape[0]
+    else: cbv_ = cbv_[:nBmax,:]
+        
+    corr_flux = np.zeros(nobs)
     corr_flux_multi = np.zeros((nBmax,nobs))
-    weights = np.zeros((nBmax,nBmax))
-    sig = 
-    corrected_flux = np.copy(flux)
+    weights_multi = np.zeros((nBmax,nBmax))
+    ran_multi = np.zeros(nBmax)
+    sig_multi = np.zeros(nBmax)
+
     l = np.isfinite(flux)
     if not use is None: l *= use
-    weights = fit_basis(flux[l].reshape((1,l.sum())), cbv_[:,l])
-    corr = apply_basis(weights, cbv_).reshape(flux.shape)
-    corrected_flux = flux - corr
-    if doPlot == True:
-        pl.clf()
-        x = np.arange(nobs)
-        pl.plot(x, flux, '-', c = 'grey')
-        pl.plot(x[l], flux[l], 'k-')
-        pl.plot(x, corr, 'c-')
-        pl.plot(x, corrected_flux, 'm-')
-        pl.xlabel('Observation number')
-        pl.xlabel('Flux')
-    return corrected_flux, weights
 
-def cbv_sel():
+    med, ran, sig = medransig(flux[l])
+    ran_raw = ran
+    sig_raw = sig
+
+    for i in range(nBmax):
+        cbv_c = cbv_[:i+1,:]
+        w_c = fit_basis(flux[l].reshape((1,l.sum())), cbv_c[:,l])
+        weights_multi[i,:i+1] = w_c
+        corr = apply_basis(w_c, cbv_c).reshape(flux.shape)
+        c = flux - corr
+        corr_flux_multi[i,:] = c
+        med, ran, sig = medransig(c[l])
+        ran_multi[i] = ran
+        sig_multi[i] = sig
+
+    # Select the best number of basis functions
+    # (smallest number that significantly reduces range)
+    med_ran = np.median(ran_multi)
+    sig_ran = 1.48 * np.median(abs(ran_multi - med_ran))
+    jj = np.where(ran_multi < med_ran + 3 * sig_ran)[0][0]
+    # Does that introduce noise? If so try to reduce nB till it doesn't
+    med_raw, ran_raw, sig_raw = medransig(flux[l])
+    if sig_multi[jj] > 1.1 * sig_raw:
+        while jj > 0:
+            jj -= 1
+            if sig_multi[jj] <= 1.1 * sig_raw: break
+    nb_opt = jj + 1
+    sig_opt = sig_multi[jj]
+    ran_opt = ran_multi[jj]
+    flux_opt = corr_flux_multi[jj,:].flatten()
+
+    if full_output:
+        return (flux_opt, nb_opt, ran_opt, sig_opt), (med_raw, sig_raw, ran_raw), \
+          (flux_corr_multi, ran_multi, sig_multi)
+    else:
+        return (flux_opt, nb_opt, ran_opt, sig_opt), (med_raw, sig_raw, ran_raw)
 
 # def correct_file(infile, cbvfile, outfile, input_type = 'SAP', \
 #                  exclude_func = None, exclude_func_par = None, doplot = False):
